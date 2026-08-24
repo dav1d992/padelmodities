15f874825fbfa4d55f4cb262f87659d3c6de8a41
dav <dav@danskecommodities.com>
2026-08-24 20:04:11 +0200

    Track environment.ts so CI build can resolve Firebase config

---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyB7SZ13SfuXO6dbnHvNiiOCDIKDgViXCcg',
+    authDomain: 'padelmodities.firebaseapp.com',
+    databaseURL: 'https://padelmodities-default-rtdb.europe-west1.firebasedatabase.app',
+    projectId: 'padelmodities',
+    storageBucket: 'padelmodities.firebasestorage.app',
+    messagingSenderId: '413086928078',
+    appId: '1:413086928078:web:fe2dbd98fda05c708fd49f',
+  },
+};
