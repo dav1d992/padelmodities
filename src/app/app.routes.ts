71d41993fbf88c68cbcae9aea075cf0d2a7a41af
dav1d992 <daviden@live.dk>
2026-09-08 21:21:21 +0200

    Refactor a lil ith pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,10 @@
 import type { Routes } from '@angular/router';
-import { LeaderboardComponent } from './components/leaderboard/leaderboard.component';
-import { PlayerDetailComponent } from './components/player-detail/player-detail.component';
-import { CreatePlayerComponent } from './components/create-player/create-player.component';
-import { TournamentSetupComponent } from './components/tournament-setup/tournament-setup.component';
-import { TournamentViewComponent } from './components/tournament-view/tournament-view.component';
-import { SimulationComponent } from './components/simulation/simulation.component';
+import { LeaderboardComponent } from './pages/leaderboard/leaderboard.component';
+import { PlayerDetailComponent } from './pages/player-detail/player-detail.component';
+import { CreatePlayerComponent } from './pages/create-player/create-player.component';
+import { TournamentSetupComponent } from './pages/tournament-setup/tournament-setup.component';
+import { TournamentViewComponent } from './pages/tournament-view/tournament-view.component';
+import { SimulationComponent } from './pages/simulation/simulation.component';
 
 export const routes: Routes = [
   { path: '', component: LeaderboardComponent, title: 'title.rank' },
